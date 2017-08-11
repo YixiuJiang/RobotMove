2cf7c606481514ebdbb2f7513807d6de058ef0e7
Yixiu Jiang <yixiu.jiang@gmail.com>
2017-08-11 23:02:57 +1000

    cleanup repo and update gradle version

--- src/main/java/com/codetest/toyrobot/GameApplication.java
+++ src/main/java/com/codetest/toyrobot/GameApplication.java
@@ -1,5 +1,20 @@
 package com.codetest.toyrobot;
 
-public class GameApplication {
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameApplication implements CommandLineRunner {
+
+  @Override
+  public void run(String... args) {
+  }
+
+
+  public static void main(String[] args) {
+    SpringApplication.run(GameApplication.class, args);
+
+  }
 }
